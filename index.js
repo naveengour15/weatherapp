c250ecce7b8b9d71c9d4670e2ebc008a913e91bb
naveengour <naveengour15@gmail.com>
2025-07-08 18:04:55 +0530

    create weather tempature with city bise in node js

---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const http = require("http");
+var requests = require("requests");
+const homeFile = fs.readFileSync("home.html", "UTF-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+    console.log("in server");
+    try{
+        if (req.url == "/") {
+            console.log("url");
+            requests(
+            "http://api.openweathermap.org/data/2.5/weather?q=Bhopal&units=metric&appid=d247507cb618703b8f1022beaa44f1a6"
+            )
+            .on("data", (chunk) => {
+                const objData = JSON.parse(chunk);
+                
+                const arrData = [objData];
+                const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
+                res.write(realTimeData);
+            })
+            .on("end", (error) => {
+                if(error) {
+                    console.log(" connection closed due to errors "+ error);
+                }
+                console.log("end");
+            });
+        }
+    } catch (error) {
+        console.error("Error occurred:", error);
+    }
+});
+
+server.listen(8000, "127.0.0.1");
